d20f363dfc808933b6942f097303dd2efb5f727a
Alexandr Smirnov <smirnov.a@rocket.red>
2026-03-07 20:09:50 +0300

    Ready

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
 import { createPinia } from 'pinia'
 import router from './router'
+import App from './App.vue'
+import './style.css'
 
 const pinia = createPinia()
 
